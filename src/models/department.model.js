49dbd9f5f9665dcd04df0726a05ef18cea6bdca8
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-02-19 12:37:58 +0300

    feat: updated department.controller

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,21 +1,21 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require("../configs/dbConnection");
+const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 
-const DepartmentSchema = new mongoose.Schema(
-  {
+const DepartmentSchema = new mongoose.Schema({
+
     name: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-  },
-  { collection: "departments", timestamps: true }
-);
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+
+}, { collection: "departments", timestamps: true })
+// collection: db name
 
 /* ------------------------------------------------------- */
-module.exports = mongoose.model("Department", DepartmentSchema);
+module.exports = mongoose.model('Department', DepartmentSchema)
