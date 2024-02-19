fae87ef49b385e462548e5a7f24907fc6a152329
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-02-19 11:49:30 +0300

    feat: updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
@@ -8,24 +8,55 @@
     $ npm i jsonwebtoken
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
+// Required Modules:
+
+// envVariables to process.env:
+require("dotenv").config();
+const PORT = process.env?.port || 8000;
+
+// asyncErrors to errorHandler:
+require("express-async-errors");
+
+/* ------------------------------------------------------- */
+// Configrations:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* ------------------------------------------------------- */
+// Middlewares:
+// Accept JSON:
+app.use(express.json());
+
+// SessionsCookies:
+app.use(require("cookie-session")({ secret: process.env.SECRET_KEY }));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+/* ------------------------------------------------------- */
+// Routes:
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PERSONNEL API",
+  });
+});
 
 
 
-// continue from here...
-
-
-
+/* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
