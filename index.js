70423c76a1737156494b357a77b7239f7b714dcc
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-02-19 13:32:51 +0300

    feat: updated personnel router

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ app.all("/", (req, res) => {
 
 // departments
 app.use("/departments", require("./src/routes/department.router"));
+// personnels
+app.use("/personnels", require("./src/routes/personnel.router"));
 
-/* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 
 // errorHandler:
