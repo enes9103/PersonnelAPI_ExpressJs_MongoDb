fba44d311e3064459fedc8e43761d5f1054ba119
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-02-19 11:56:50 +0300

    feat: updated department model

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,19 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const DepartmentScheme = new mongoose.Scheme(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { collection: "departments", timestamps: true }
+);
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Department", DepartmentScheme);
